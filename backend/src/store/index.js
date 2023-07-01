4cf9836ecb18196525153e6fbc41caaacf941bcf
moaaz97 <moaaz.briek97@gmail.com>
2023-07-01 19:41:48 +0300

    Store index

---
+++ backend/src/store/index.js
@@ -0,0 +1,10 @@
+import { createRouter } from 'vue-router'
+import {createWebHistory} from 'vue-router'
+
+const routes = [];
+const router = createRouter ({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
